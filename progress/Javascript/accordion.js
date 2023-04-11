b49f9995701078d63bd30847fdfe9c91a6e598dd
Ndobito <jcuellarmenza@gmail.com>
2023-04-10 22:23:27 -0500

    Interfaces - Complete 100%

--- progress/Javascript/accordion.js
+++ progress/Javascript/accordion.js
@@ -2,7 +2,6 @@
 /*Accordion*/
 
 const slideAccordion = document.querySelectorAll(".container-accordion-slide"); 
-
 slideAccordion.forEach( ac => {
     ac.addEventListener("click", () => {
         ac.classList.toggle("active");
--- progress/Javascript/form-disable-enable.js
+++ progress/Javascript/form-disable-enable.js
@@ -1,22 +1,45 @@
-function enable(){
-    var inputs =document.getElementsByTagName("input");
-    var selects =document.getElementsByTagName("select");
+// form-User-Information
+document.getElementById("enableForm1").addEventListener("click", function() {
+    enableForm("form-user-information");
+  });
+  
+  document.getElementById("disableForm1").addEventListener("click", function() {
+    disableForm("form-user-information");
+  });
+  
+  // Habilitar y deshabilitar form2
+  document.getElementById("enableForm2").addEventListener("click", function() {
+    enableForm("form-pet-information");
+  });
+  
+  document.getElementById("disableForm2").addEventListener("click", function() {
+    disableForm("form-pet-information");
+  });
+  
+  // Funciones para habilitar y deshabilitar campos del formulario
+  function enableForm(formId) {
+    var inputs = document.getElementById(formId).getElementsByTagName("input");
+    var selects = document.getElementById(formId).getElementsByTagName("select");
+    
     for (var i = 0; i < inputs.length; i++) {
-        inputs[i].disabled = false;
+      inputs[i].disabled = false;
     }
+    
     for (var i = 0; i < selects.length; i++) {
-        selects[i].disabled = false;
+      selects[i].disabled = false;
     }
-}
-
-
-function disable() {
-    var inputs =document.getElementsByTagName("input");
-    var selects =document.getElementsByTagName("select");
+  }
+  
+  function disableForm(formId) {
+    var inputs = document.getElementById(formId).getElementsByTagName("input");
+    var selects = document.getElementById(formId).getElementsByTagName("select");
+    
     for (var i = 0; i < inputs.length; i++) {
-        inputs[i].disabled = true;
+      inputs[i].disabled = true;
     }
+    
     for (var i = 0; i < selects.length; i++) {
-        selects[i].disabled = true;
+      selects[i].disabled = true;
     }
-}
+  }
+  
