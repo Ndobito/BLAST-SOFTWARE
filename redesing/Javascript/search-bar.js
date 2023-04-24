f4426c5417e5c0d6183cf1485b8c25d22f56be18
Ndobito <jcuellarmenza@gmail.com>
2023-04-24 18:35:38 -0500

    Index - Hero

--- redesing/Javascript/search-bar.js
+++
@@ -1,8 +0,0 @@
-const bar = document.getElementById("ctSearch"); 
-const icon = document.getElementById("icon-search");
-
-icon.addEventListener("click", () => {
-    bar.style.visibility = "visible"; 
-    bar.style.maxWidth = "100%";  
-    icon.style.marginLeft = "40%"; 
-}); 
