fd262088f28eaff81527e0e754daf3356f53957b
arcelot27 <arctamua@gmail.com>
2023-04-26 18:22:01 -0500

    actualización

--- redesing/Javascript/contact.js
+++ redesing/Javascript/contact.js
@@ -1,21 +1,19 @@
-
-const form = document.getElementById('contact-form');
-const modal = document.querySelector('.modal');
-const closeBtn = document.getElementById('close');
-const opacidad = document.querySelector('.opacidad');
-
-form.addEventListener('submit', (event) => {
+const enviarBtn = document.getElementById("enviar-btn");
+const modal = document.querySelector(".modal");
+const opacidad = document.querySelector(".opacidad");
+const cerrarBtn = document.getElementById("close");
+enviarBtn.addEventListener("click", function(event) {
   event.preventDefault();
-
-  if (form.checkValidity()) {
-    // Si todos los campos requeridos están llenos, mostrar la ventana emergente
-    modal.style.display = 'block';
-    opacidad.style.display = 'block';
+  const isFormValid = ["email", "message"].every(id => {
+    const inputField = document.getElementById(id);
+    return inputField.value.trim() !== "";
+  });
+  if (isFormValid) {
+    modal.style.display = "block";
+    opacidad.style.display = "block";
   }
 });
-
-closeBtn.addEventListener('click', () => {
-  // Ocultar la ventana emergente cuando se hace clic en el botón 'Volver'
-  modal.style.display = 'none';
-  opacidad.style.display = 'none';
+cerrarBtn.addEventListener("click", function() {
+  modal.style.display = "none";
+  opacidad.style.display = "none";
 });
