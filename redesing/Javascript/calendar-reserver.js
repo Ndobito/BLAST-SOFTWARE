17fe399bded5f75466bd4bdadd9e2851c572582b
Ndobito <jcuellarmenza@gmail.com>
2023-04-27 15:53:58 -0500

    Restore Services

--- redesing/Javascript/calendar-reserver.js
+++ redesing/Javascript/calendar-reserver.js
@@ -1,32 +1,91 @@
-function generateCurrentMonthCalendar() {
-    const today = new Date();
-    const year = today.getFullYear()
-    const month = today.getMonth();
-    generateCalendar(year, month);
-  
-}
-function generateCalendar(year, month) {
-  const daysInMonth = new Date(year, month + 1, 0).getDate();
-  const startDay = new Date(year, month, 1).getDay();
-  
-  const tbody = document.getElementById('calendar-body');
-  tbody.innerHTML = '';
-  
-  let date = 1;
-  for (let i = 0; i < 6; i++) {
-    const row = document.createElement('tr');
-    for (let j = 0; j < 7; j++) {
-      const cell = document.createElement('td');
-      if (i === 0 && j < startDay) {
-        cell.innerText = '';
-      } else if (date > daysInMonth) {
-        cell.innerText = '';
-      } else {
-        cell.innerText = date;
-        date++;
-      }
-      row.appendChild(cell);
-    }
-    tbody.appendChild(row);
+function containerCalendar(container) {
+
+  if (container.textContent == "") {
+    
+  }else{
+    
+
   }
+  
+
+  const year = new Date().getFullYear();
+
+  const months = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"
+  ];
+
+  const daysOfWeek = ["Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb"];
+
+  const calendarContainer = document.getElementById(container);
+
+  for (let i = 0; i < months.length; i++) {
+    const slideContainer = document.createElement("div");
+    slideContainer.classList.add("slide");
+    calendarContainer.appendChild(slideContainer);
+
+    const table = document.createElement("table");
+
+    const caption = document.createElement("caption");
+    const captionText = document.createTextNode(months[i] + " " + year);
+    caption.appendChild(captionText);
+    table.appendChild(caption);
+
+    const daysOfWeekRow = document.createElement("tr");
+
+    for (let j = 0; j < daysOfWeek.length; j++) {
+      const cell = document.createElement("th");
+      const dayOfWeekName = document.createTextNode(daysOfWeek[j]);
+      cell.appendChild(dayOfWeekName);
+      daysOfWeekRow.appendChild(cell);
+    }
+
+    table.appendChild(daysOfWeekRow);
+
+    const daysInMonth = new Date(year, i + 1, 0).getDate();
+    const firstDayOfWeek = new Date(year, i, 1).getDay();
+
+    let date = 1;
+    for (let j = 0; j < 6; j++) {
+      const weekRow = document.createElement("tr");
+
+      for (let k = 0; k < 7; k++) {
+        const cell = document.createElement("td");
+
+        if (j === 0 && k < firstDayOfWeek || date > daysInMonth) {
+          const dayNumber = document.createTextNode("");
+          cell.appendChild(dayNumber);
+          if (cell == "") {
+
+          } else {
+            cell.classList.add(k);
+          }
+        } else {
+          const dayNumber = document.createTextNode(date);
+          cell.appendChild(dayNumber);
+          date++;
+        }
+
+        weekRow.appendChild(cell);
+      }
+
+      table.appendChild(weekRow);
+    }
+
+    slideContainer.appendChild(table);
+  }
+
+  //Calendar
+  
+  
 }
