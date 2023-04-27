5887b1e2741cf8f1fb8acc4796ac9e2da31d1a24
ronaldo07_rgb <roseroronaldo07@gmail.com>
2023-04-26 22:05:58 -0500

    update

--- redesing/Javascript/contact.js
+++ redesing/Javascript/contact.js
@@ -2,18 +2,18 @@ const enviarBtn = document.getElementById("enviar-btn");
 const modal = document.querySelector(".modal");
 const opacidad = document.querySelector(".opacidad");
 const cerrarBtn = document.getElementById("close");
-enviarBtn.addEventListener("click", function(event) {
-  event.preventDefault();
-  const isFormValid = ["email", "message"].every(id => {
-    const inputField = document.getElementById(id);
-    return inputField.value.trim() !== "";
-  });
-  if (isFormValid) {
-    modal.style.display = "block";
-    opacidad.style.display = "block";
-  }
+enviarBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    const isFormValid = ["email", "message"].every(id => {
+        const inputField = document.getElementById(id);
+        return inputField.value.trim() !== "";
+    });
+    if (isFormValid) {
+        modal.style.display = "block";
+        opacidad.style.display = "block";
+    }
 });
-cerrarBtn.addEventListener("click", function() {
-  modal.style.display = "none";
-  opacidad.style.display = "none";
+cerrarBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+    opacidad.style.display = "none";
 });
