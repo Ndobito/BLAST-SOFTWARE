529617acf04b9d9bf4990e83dc6278cf9407e87f
ronaldo07_rgb <roseroronaldo07@gmail.com>
2023-04-27 14:40:32 -0500

    update-NewAcoount

--- redesing/Javascript/New-Account.js
+++ redesing/Javascript/New-Account.js
@@ -3,13 +3,14 @@ const petContainer = document.getElementById("parent-container-form-pet");
 const botonPet = document.querySelector(".nextpet");
 const botonReturn = document.querySelector(".return");
 
+botonPet.addEventListener("click", (event) => {
+    event.preventDefault()
+    petContainer.style.display = "block";
+    userContainer.style.display = "none";
+});
+
 botonReturn.addEventListener("click", (event) => {
     event.preventDefault();
     petContainer.style.display = "none";
     userContainer.style.display = "block";
-});
-
-botonPet.addEventListener("click", () => {
-    petContainer.style.display = "block";
-    userContainer.style.display = "none";
 });
--- redesing/Javascript/services.js
+++ redesing/Javascript/services.js
@@ -29,7 +29,6 @@ function reserver(opc){
 
     switch (opc) {
         case 1:
-            panel1.classList.add("reserve-active");
             panel1.style.display = "block";
             break;
         case 2:
@@ -47,4 +46,39 @@ function reserver(opc){
     }
 }
 
-//Calendar 
+// Calendar 
+
+function generateCurrentMonthCalendar() {
+    const today = new Date();
+    const year = today.getFullYear()
+    const month = today.getMonth();
+    generateCalendar(year, month);
+  
+}
+function generateCalendar(year, month) {
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+  const startDay = new Date(year, month, 1).getDay();
+  
+  const tbody = document.getElementById('calendar-body');
+  tbody.innerHTML = '';
+  
+  let date = 1;
+  for (let i = 0; i < 6; i++) {
+    const row = document.createElement('tr');
+    for (let j = 0; j < 7; j++) {
+      const cell = document.createElement('td');
+      if (i === 0 && j < startDay) {
+        cell.innerText = '';
+      } else if (date > daysInMonth) {
+        cell.innerText = '';
+      } else {
+        cell.innerText = date;
+        date++;
+      }
+      row.appendChild(cell);
+    }
+    tbody.appendChild(row);
+  }
+}
+
+
