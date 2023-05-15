0bb5fdc1ef9e2d9ff00d13a8c55cc8604697c900
Ronaldo07rgr <roseroronaldo07@gmail.com>
2023-05-15 08:37:50 -0500

    update-administrator

--- redesing/Javascript/menu-profile-administrator.js
+++ redesing/Javascript/menu-profile-administrator.js
@@ -1,22 +1,31 @@
 const container1 = document.getElementById("container-right");
 const container2 = document.getElementById("container-right2");
 const container3 = document.getElementById("container-right3");
+const container4 = document.getElementById("container-right4");
 
 
 function container(n) {
 
     if ( n == 1 ) {
         container2.style.display = "none"; 
+        container4.style.display = "none"; 
         container3.style.display = "none"; 
         container1.style.display = "block";
     } else if ( n == 2 ) {
         container1.style.display = "none"; 
+        container4.style.display = "none"; 
         container3.style.display = "none"; 
         container2.style.display = "block";
     } else if ( n == 3 ) {
         container1.style.display = "none"; 
-        container2.style.display = "none"; 
+        container2.style.display = "none";
+        container4.style.display = "none"; 
         container3.style.display = "block";
+    }else if ( n == 4 ) {
+        container1.style.display = "none"; 
+        container2.style.display = "none"; 
+        container3.style.display = "none";
+        container4.style.display = "block";
     }
-    
+        
 }
