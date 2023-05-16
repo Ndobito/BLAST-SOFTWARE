7f9c23c7ead1663e792f9058790eef83735a99ae
arcelot27 <arctamua@gmail.com>
2023-05-16 11:38:38 -0500

    actualización

--- redesing/php/login.php
+++ redesing/php/login.php
@@ -1,18 +1,17 @@
 <?php
-echo "hola Mundo"; 
-include 'conexion.php';
-$conectar = $conexion; 
-if(isset($_POST['submit'])){
-    $id = $_POST['ctUser']; 
-    $pass = $_POST['ctPassword']; 
+    include 'conexion.php';
+    $conectar = $conexion; 
+    if(isset($_POST['submit'])){
+        $id = $_POST['ctUser']; 
+        $pass = $_POST['ctPassword']; 
 
-    $consulta = "SELECT * FROM cliente"; 
+        $consulta = "SELECT * FROM cliente where usercli='$id' AND passcli='$pass' "; 
 
-    $mostrar = mysqli_query($conectar, $consulta);
-    echo "Conexion Exitosa"; 
-    // while($result = mysqli_fetch_array($mostrar)){
-    //     echo "Hola";   
-    // }
-}
+        $mostrar = mysqli_query($conectar, $consulta);
+        echo "Conexion Exitosa"; 
+        // while($result = mysqli_fetch_array($mostrar)){
+        //     echo "Hola";   
+        // }
+    }
 
 ?>
