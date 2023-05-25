3b1451be5354fa7ccc363b17e36e5aa0de2f49b5
arcelot27 <arctamua@gmail.com>
2023-05-25 06:41:35 -0500

    actualización

---
+++ MVC/view/Javascript/new-account.js
@@ -0,0 +1,15 @@
+const userContainer = document.getElementById("parent-container-form-user");
+const petContainer = document.getElementById("parent-container-form-pet");
+const botonPet = document.querySelector(".nextpet");
+const botonReturn = document.querySelector(".return1");
+
+botonPet.addEventListener("click", () => {
+    petContainer.style.display = "block";
+    userContainer.style.display = "none";
+});
+
+botonReturn.addEventListener("click", (event) => {
+    event.preventDefault();
+    petContainer.style.display = "none";
+    userContainer.style.display = "block";
+});
