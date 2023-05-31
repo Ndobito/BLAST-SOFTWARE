228ce7e79a1cb460ca5c76b0af2255c66aa8d463
Ndobito <jcuellarmenza@gmail.com>
2023-05-31 11:35:53 -0500

    Avance en MVC

--- MVC/controller/index.controller.php
+++ MVC/controller/index.controller.php
@@ -12,6 +12,7 @@ class indexController{
     public function Inicio(){
         $style = "<link rel='stylesheet' href='assets/css/style-index.css'>"; 
         require_once "view/head.php"; 
+        require_once "view/header.php"; 
         require_once "view/index/index.php"; 
         require_once "view/footer.php";
     }
