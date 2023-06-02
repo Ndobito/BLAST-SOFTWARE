b82b191fbcbe0bb933b1018dbf10807f44891715
ronaldo07_rgb <roseroronaldo07@gmail.com>
2023-06-01 19:02:31 -0500

    prueba

---
+++ MVC/controller/know-us.controller.php
@@ -0,0 +1,12 @@
+<?php
+
+class knowUsController{
+
+    public function knowUs(){
+        $style = "<link rel='stylesheet' href='assets/css/style-know-us.css'>"; 
+        require_once "view/head.php"; 
+        require_once "view/know-us/know-us.php"; 
+        require_once "view/footer.php"; 
+    }
+
+}
