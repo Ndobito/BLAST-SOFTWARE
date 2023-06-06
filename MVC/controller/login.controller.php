016109a821d9e4070c34b9aa1669c39585f76b20
arcelot27 <arctamua@gmail.com>
2023-06-06 11:23:24 -0500

    actualización

--- MVC/controller/login.controller.php
+++ MVC/controller/login.controller.php
@@ -1,46 +1,46 @@
 <?php
 
 include_once "model/login.php";
-class loginController{
-    private $object;
 
-    public function __construct(){
-        $this -> object = new Login(); 
+class LoginController {
+    private $loginModel;
+
+    public function __construct() {
+        $this->loginModel = new Login();
+        session_start(); // Iniciar la sesión
     }
 
-    public function Inicio(){
+    public function inicio() {
         $style = "<link rel='stylesheet' href='assets/css/style-login.css'>"; 
         require_once "view/head.php"; 
         require_once "view/login/login.php"; 
     }
-    public function revicion(){
-        $user = $_POST['ctUser'];
-        $password = $_POST['ctPassword'];
+
+    public function revicion() {
+        $usuario = $_POST['ctUser'];
+        $passsword = $_POST['ctPassword'];
         $terminos = $_POST['checkbox'];
-        // $_SESSION[]
-        if(empty($terminos) || empty($user) || empty($password)){
-             
-            echo "<p>se requiere rellenar epacios";
-        }else{
-            
-            if($user){
-                $validacion = "SELECT*FROM user, usercli";
-            }else{
-                echo "<p>nombre de usuario incorrecto</p>";
+    
+        if (empty($terminos) || empty($usuario) || empty($passsword)) {
+            echo "<p>Se requiere llenar todos los campos.</p>";
+        } else {
+            $usuario_valido = $this->loginModel->validarUsuario($usuario, $passsword);
+    
+            if ($usuario_valido) {
+                // redirigir a perfil del usuario
+                header('Location: http://localhost:8080/BLAST-SOFTWARE/MVC/?b=knowus');
+                exit();
+            } else {
+                $usuario_registrado = $this->loginModel->existeUsuario($usuario);
+                
+                if ($usuario_registrado) {
+                    echo "<p>Contraseña incorrecta.</p>";
+                } else {
+                    echo "<p>Usuario no registrado.</p>";
+                }
             }
-
-            
-            if($password){
-                $validacion = "SELECT*FROM user, passcli";
-            }else{
-                echo "<p>contraseña de usuario incorrecto</p>";
-            }
-
         }
-
-
     }
-
+    
 }
-
 ?>
