3c39e23b6db6c7c9760fa35e65e38efe4d1575e5
ronaldo07_rgb <roseroronaldo07@gmail.com>
2023-06-12 14:23:32 -0500

    update

--- MVC/controller/contact-us.php
+++ MVC/controller/contact-us.php
@@ -5,37 +5,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $email = $_POST['email'];
   $mensaje = $_POST['mensaje'];
 
-<<<<<<< HEAD
-    class Contact{
-        
-        public function Contact(){
-            $style = "<link rel='stylesheet' href='assets/css/style-contact-us.css'>"; 
-            require_once "view/head.php"; 
-            require_once "view/index/index.php"; 
-            require_once "view/footer.php"; 
-        } 
-        
-        public function suggestions(){
-            $name = $_POST['name'];
-            $surnames = $_POST['surname'];
-            $username = $_POST['username'];
-            $message =$_POST['message'];
-                // Registrar la sugerencia   en la base de datos
-            $sql = "SELECT username FROM contact WHERE username = $username";
-            $stmt = Database::Conectar()->prepare($sql);
-            $stmt->execute();
-                if ($stmt->rowCount() > 0) {
-                    $sql = "INSERT INTO sigup (name, surname, username, message) VALUES (?, ?, ?, ?)";
-                    $stmt = Database::Conectar()->prepare($sql);
-                    echo "Se a enviado la sugerencia correctamente";         
-                } 
-                else {
-                    echo "Error al enviar sugerencia por  favor registrese o inicie secion: " . $stmt->error;
-                }
-
-        }
-    }
-=======
   // Configuración del correo
   $destinatario = 'Animal2023world@gmail.com';
   $asunto = 'sugerencias';
@@ -56,5 +25,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo 'Ha ocurrido un error al enviar el mensaje.';
   }
 }
->>>>>>> 6f616153bea1047cf40b3e088b62c25f48ddd063
 ?>
--- MVC/model/login.php
+++ MVC/model/login.php
@@ -1,18 +1,33 @@
 <?php
 
- class Login{   
-    private $consulta;
+class Login {
+    private $conexion;
 
-    public  $user, $pass;
-    
-    public function __construct(){
-        try{
-            $this -> consulta = databaseConexion::conexion();
-        }catch(PDOException $e){
-            echo "Error de Conexion ". $e -> getMessage(); 
+    public function __construct() {
+        $this->conexion = databaseConexion::conexion();
+    }
+
+    public function validarUsuario($usuario, $contrasena) {
+        $query = "SELECT * FROM cliente WHERE usercli = '$usuario' AND passcli = '$contrasena'";
+        $resultado = mysqli_query($this->conexion, $query);
+
+        if (mysqli_num_rows($resultado) > 0) {
+            return true; // El usuario y la contraseña son válidos
+        } else {
+            return false; // El usuario o la contraseña son incorrectos
         }
     }
- }
 
+    public function existeUsuario($usuario) {
+        $query = "SELECT * FROM cliente WHERE usercli = '$usuario'";
+        $resultado = mysqli_query($this->conexion, $query);
+
+        if (mysqli_num_rows($resultado) > 0) {
+            return true; // El usuario está registrado
+        } else {
+            return false; // El usuario no está registrado
+        }
+    }
+}
 
 ?>
