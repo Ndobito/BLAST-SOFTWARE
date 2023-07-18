9e45d26f0c927f5d5441277d4c2c97cefcc175d0
arcelot27 <arctamua@gmail.com>
2023-07-18 11:46:20 -0500

    x

--- MVC/controller/profileadministrador.controller.php
+++ MVC/controller/profileadministrador.controller.php
@@ -18,10 +18,12 @@ class ProfileController
         $model = new Profile();
         $administrador = $model->selectUser($nombreUsuario);
 
-        
-        
-
-        
+        if ($administrador !== null) {
+            $data = compact('administrador');
+            require_once "view/profile/admin/profileadministrador.php";
+        } else {
+           
+        }
 
         require_once "view/footerprofile.php";
     }
@@ -35,8 +37,8 @@ class ProfileController
 
 
     //BUSCADOR
-
-
+    public function buscador(){}
+    
 }
 
 $profileAdminController = new Profile();
