aae54e403a781a93531bc62c3fe89c69ca30e413
Ndobito <jcuellarmenza@gmail.com>
2023-08-22 11:46:26 -0500

    x

--- MVC/view/book-appointment/book-appointment.php
+++ MVC/view/book-appointment/book-appointment.php
@@ -4,83 +4,52 @@
                 <h1>¡No podemos esperar a verte!</h1>
             </div>
             <div class="container-services">
-                <div class="cont-service citas">
-                    <div class="service">
+                <div class="container-service">
+                    <div>
                         <h1>Citas</h1>
-                        <p>1h <br> $10.000</p>
-                        <button onclick="reserver(1)">Reservar</button>
+                        <div class="circle-icon"><i class="fa-regular fa-calendar-check"></i></div>
                     </div>
-                    <div class="reserve" id="reserve-citas">
-                        <i class="fa-solid fa-xmark" onclick="exit(1)"></i>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus recusandae minus harum odit voluptatibus nobis odio eaque eos ex inventore suscipit, commodi accusamus esse magnam officiis error ipsa quasi! Assumenda.</p>
+                    </div>
+                    <div>
+                        <button>Agendar Cita</button>
+                    </div>
+                </div>
+                <div class="container-service">
+                    <div>
                         <h1>Citas</h1>
-                        <p>Consulta nuestra disponibilidad y reserva la fecha y hora que más te convenga</p>
-
-                        <div class="container-calendar">
-                            <div class="slider-calendar" id="calendar-reserve1"></div> 
-                            <div id="calendar-citas1"></div>
-                        </div>
-
-                        <button>Confirmar</button>
+                        <div class="circle-icon"><i class="fa-regular fa-calendar-check"></i></div>
+                    </div>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus recusandae minus harum odit voluptatibus nobis odio eaque eos ex inventore suscipit, commodi accusamus esse magnam officiis error ipsa quasi! Assumenda.</p>
+                    </div>
+                    <div>
+                        <button>Agendar Cita</button>
                     </div>
                 </div>
-                <div class="cont-service">
-                    <div class="service">
-                        <h1>Cirugia</h1>
-                        <p>1h <br> $10.000</p>
-                        <button onclick="reserver(2)">Reservar</button>
+                <div class="container-service">
+                    <div>
+                        <h1>Citas</h1>
+                        <div class="circle-icon"><i class="fa-regular fa-calendar-check"></i></div>
                     </div>
-                    <div class="reserve" id="reserve-surgeries">
-                        <i class="fa-solid fa-xmark" onclick="exit(2)"></i>
-                        <h1>Cirugia</h1>
-                        <p>Consulta nuestra disponibilidad y reserva la fecha y hora que más te convenga</p>
-
-                        <div class="container-calendar">
-                            <div class="slider-calendar" id="calendar-reserve2"></div> 
-                            <div id="calendar-citas"></div>
-                        </div>
-
-                        <button>Confirmar</button>
-
+                    <div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus recusandae minus harum odit voluptatibus nobis odio eaque eos ex inventore suscipit, commodi accusamus esse magnam officiis error ipsa quasi! Assumenda.</p>
+                    </div>
+                    <div>
+                        <button>Agendar Cita</button>
                     </div>
                 </div>
-                <div class="cont-service">
-                    <div class="service">
-                        <h1>Laboratorio Clinico</h1>
-                        <p>1h <br> $10.000</p>
-                        <button onclick="reserver(3)">Reservar</button>
+                <div class="container-service">
+                    <div>
+                        <h1>Citas</h1>
+                        <div class="circle-icon"><i class="fa-regular fa-calendar-check"></i></div>
                     </div>
-                    <div class="reserve" id="reserve-lab">
-                        <i class="fa-solid fa-xmark" onclick="exit(3)"></i>
-                        <h1>Laboratorio Clinico</h1>
-                        <p>Consulta nuestra disponibilidad y reserva la fecha y hora que más te convenga</p>
-
-                        <div class="container-calendar">
-                            <div class="slider-calendar" id="calendar-reserve3"></div> 
-                            <div id="calendar-citas"></div>
-                        </div>
-
-                        <button>Confirmar</button>
-
+                    <div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus recusandae minus harum odit voluptatibus nobis odio eaque eos ex inventore suscipit, commodi accusamus esse magnam officiis error ipsa quasi! Assumenda.</p>
                     </div>
-                </div>
-                <div class="cont-service">
-                    <div class="service">
-                        <h1>Radiografias</h1>
-                        <p>1h <br> $10.000</p>
-                        <button onclick="reserver(4)">Reservar</button>
-                    </div>
-                    <div class="reserve" id="reserve-xrays">
-                        <i class="fa-solid fa-xmark" onclick="exit(4)"></i>
-                        <h1>Radiografias</h1>
-                        <p>Consulta nuestra disponibilidad y reserva la fecha y hora que más te convenga</p>
-
-                        <div class="container-calendar">
-                            <div class="slider-calendar" id="calendar-reserve4"></div> 
-                            <div id="calendar-citas"></div>
-                        </div>
-
-                        <button>Confirmar</button>
-
+                    <div>
+                        <button>Agendar Cita</button>
                     </div>
                 </div>
             </div>
